4263717f92b74366cc93e03fcea6079992ae560a
Christian Schulze <christian.schulze@sma.de>
2020-04-24 07:40:05 +0200

    add fix

--- DockerExample/Startup.cs
+++ DockerExample/Startup.cs
@@ -14,7 +14,7 @@ namespace DockerExample
         {
             Configuration = configuration;
         }
-
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -38,8 +38,7 @@ namespace DockerExample
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
